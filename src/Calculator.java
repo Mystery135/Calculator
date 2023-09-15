637c085091a475038e4bd80236f057d3571b8449
Mystery135 <websiteemail12345678910@gmail.com>
2023-09-15 11:35:42 -0700

    Inital commite

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+//    You are to create an interactive "calculator"
+//    that computes three interesting things using methods
+//    (binomial expansion, quadratic equation, drawing objects or
+//    designs of different sizes, calculating taxes and tips on dinners, etc.).
+//    You are to ask for user input as to which calculation/operation to perform as well as,
+//    possibly, input required parameters.
+//    Inclusion of all elements: 10 points
+//    Does the program work? 2 points
+//    Is the program easy to follow? 3 points
+//    Is the program streamlined (efficient use of code)? 5 points
+
+    public static void main(String[] args) {
+
+    }
+
+    private void computeQuadratic(){
+
+    }
+    private void computeTax(){
+
+    }
+    private void compute()
+
+
 }
