3ee1231dcbeea71b3c8ae4ea16bbdf3de626b45f
Mystery135 <websiteemail12345678910@gmail.com>
2023-09-17 23:25:03 -0700

    Added user input system

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Calculator {
 //    You are to create an interactive "calculator"
@@ -11,29 +11,52 @@ public class Calculator {
 //    Does the program work? 2 points
 //    Is the program easy to follow? 3 points
 //    Is the program streamlined (efficient use of code)? 5 points
-
+public static final String COMPUTE_PASCALS_TRIANGLE = "pascal's triangle";
+public static final String COMPUTE_QUADRATIC = "quadratic";
+public static final String COMPUTE_COMPOUND_INTEREST = "compound interest";
+public static final Set<String> COMPUTE_TYPES = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+                    COMPUTE_PASCALS_TRIANGLE,
+                    COMPUTE_QUADRATIC,
+                    COMPUTE_COMPOUND_INTEREST
+            )));
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String option = scanner.nextLine();
-        boolean flag = false;
-        while (flag == false){
-            System.out.println("Type out \"option 1\", \"option 2\", ");
-            option = scanner.nextLine();
-
-        }
+        String optionPicked = "";
+        do {
+            System.out.print("Choose and type in an option to compute: ");
+            int i = 0;
+            for (String string : COMPUTE_TYPES) {
+                i++;
+                System.out.print(string);
+                if (i != COMPUTE_TYPES.size()) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+            String userInput = scanner.nextLine();
+            for (String string : COMPUTE_TYPES) {
+                if (userInput.equalsIgnoreCase(string)) {
+                    optionPicked = string;
+                    break;
+                }
+            }
+        } while (optionPicked.equals(""));
+        System.out.println("You picked " + optionPicked + "!");
     }
 
     private void computeQuadratic(){
 
     }
-    private void computeTax(){
+    private void computePascalsTriangle(){
 
     }
-    private void compute(){
+    private void computeCompoundInterest(){
 
 
 
     }
+
+
 
 
 }
